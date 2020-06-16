91eb4b8dcf7a88ad33d2e8df087a7fe105bddce8
Kayla-SA-Williams <shalisha.angelique@gmail.com>
2020-06-16 14:42:17 -0400

    Update app.js
    -- add lenderGenerate to app.js to render on page load
    -- switch order of input on lender form.

--- src/lenderForm.js
+++ src/lenderForm.js
@@ -1,24 +1,34 @@
 import React from 'react'
 
 import Form from 'react-bootstrap/Form'
-
 // form to be generated on lenderGenerate
 
 const LenderForm = ({ lender, handleSubmit, handleChange, cancelPath }) => (
-  <div className='row'>
       <div className="col-sm-10 cold-md-8 mx-auto mt-5">
-        <Form onSubmit={handleSubmit}>
-          <Form.Group controlId='loan size'>
-            <Form.Label>Loan Size</Form.Label>
-            <input
-              placeholder="$450,000"
-              value={lender.loanSize}
-              name="loanSize"
-              required
-              onChange={handleChange}
-            />
-          </Form.Group>
+        <Form className= 'lenderForm' onSubmit={handleSubmit}>
+        <Form.Row className='loanFormRow'>
+            <Form.Group controlId='loan size'>
+              <Form.Label>Loan Size</Form.Label>
+              <input
+                placeholder="$450,000"
+                value={lender.loanSize}
+                name="loanSize"
+                required
+                onChange={handleChange}
+              />
+            </Form.Group>
 
+            <Form.Group controlId='credit score'>
+              <Form.Label>Credit Score</Form.Label>
+              <input
+                placeholder="680"
+                value={lender.creditScore}
+                name="creditScore"
+                onChange={handleChange}
+              />
+            </Form.Group>
+          </Form.Row>
+          <Form.Row className='loanFormRow'>
           <Form.Group controlId='property type'>
             <Form.Label>Property Type</Form.Label>
             <Form.Control as="select"
@@ -27,42 +37,31 @@ const LenderForm = ({ lender, handleSubmit, handleChange, cancelPath }) => (
             required
             placeholder=''
             onChange={handleChange}
-          >
+            >
             <option>Single Family</option>
             <option>Condo</option>
             <option>TownHouse</option>
             <option>Multifamily</option>
-          </Form.Control>
+            </Form.Control>
           </Form.Group>
 
-          <Form.Group controlId='credit scor'>
-            <Form.Label>Credit Score</Form.Label>
-            <input
-              placeholder="680"
-              value={lender.creditScore}
-              name="creditScore"
+            <Form.Group controlId='occupancy'>
+              <Form.Label>Occupancy</Form.Label>
+              <Form.Control as="select"
+              value={lender.occupancy}
+              name='occupancy'
+              required
+              placeholder=''
               onChange={handleChange}
-            />
-          </Form.Group>
-
-          <Form.Group controlId='occupancy'>
-            <Form.Label>Occupancy</Form.Label>
-            <Form.Control as="select"
-            value={lender.occupancy}
-            name='occupancy'
-            required
-            placeholder=''
-            onChange={handleChange}
-          >
-            <option>Primary</option>
-            <option>Secondary</option>
-            <option>Investment</option>
-          </Form.Control>
-          </Form.Group>
-
-          <button className='showQuotes' type="button">Quote Rates</button>
+            >
+              <option>Primary</option>
+              <option>Secondary</option>
+              <option>Investment</option>
+            </Form.Control>
+            </Form.Group>
+          </Form.Row>
+            <button className='showQuotes' type="button">Quote Rates</button>
         </Form>
       </div>
-    </div>
 )
 export default LenderForm
